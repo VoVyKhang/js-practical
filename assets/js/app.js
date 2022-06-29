a6ad276ab4f74e29f4af829eb7c9113c891a70eb
VoVyKhang <vo.vykhang24@gmail.com>
2022-06-29 15:58:41 +0700

    Image Gallery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,58 @@
-const closeIcon = document.querySelector('.modal__header-icon')
-const openModalBtn = document.querySelector('.open-modal-btn')
-const closeModalBtn = document.querySelector('.modal__footer-close')
-const modal = document.querySelector('.modal') 
+const image = document.querySelectorAll('.image img')
+const prevBtn = document.querySelector('.prev')
+const rightBtn = document.querySelector('.right')
+const closeBtn = document.querySelector('.close')
+const galleryImage = document.querySelector('.gallery__inner img')
+const gallery = document.querySelector('.gallery')
 
-function toggleModal(){
-    modal.classList.toggle('hide')
+var currentIndex = 0
+
+function showGallery() {
+    if(currentIndex == 0){
+        prevBtn.classList.add('hide')
+    }
+    else{
+        prevBtn.classList.remove('hide') 
+    }
+
+    if(currentIndex == image.length -1){
+        rightBtn.classList.add('hide')
+    }else{
+        rightBtn.classList.remove('hide')
+    }
+
+    galleryImage.src = image[currentIndex].src
+    gallery.classList.add('show')
 }
 
-openModalBtn.addEventListener('click', toggleModal)
-closeModalBtn.addEventListener('click', toggleModal)
-closeIcon.addEventListener('click', toggleModal)
+image.forEach((item, index) => {
+    item.addEventListener('click', function() {
+        currentIndex = index
+        showGallery()
+    })
+})
 
+closeBtn.addEventListener('click', function() {
+    gallery.classList.remove('show')
+
+})
+
+document.addEventListener('keydown', function(e) {
+    if(e.keyCode == 27){
+    gallery.classList.remove('show')
+    }
+})
+
+prevBtn.addEventListener('click', function() {
+    if(currentIndex > 0) {
+        currentIndex--
+        showGallery()
+    }
+})
+
+rightBtn.addEventListener('click', function() {
+    if(currentIndex < image.length -1){
+        currentIndex++
+        showGallery()
+    }
+})
